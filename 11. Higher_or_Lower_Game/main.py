fcad0517f533d3b7c1db3b517ba39dd1e571eca9
anupriyalathey <anupriyalathey@gmail.com>
2025-02-19 09:09:00 +0530

    added project 12

--- 11. Higher_or_Lower_Game/main.py
+++ 11. Higher_or_Lower_Game/main.py
@@ -1,4 +1,3 @@
-# Display art
 from art import logo, vs
 from game_data import data
 import random
@@ -23,13 +22,10 @@ def check_answer(user_guess, a_followers, b_followers):
 print(logo)
 score = 0
 game_should_continue = True
-# Generate a random account from the game data
 account_b = random.choice(data)
 
-# Make the game repeatable.
 while game_should_continue:
 
-    # Making account at position B become the next account at position A.
     account_a = account_b
     account_b = random.choice(data)
 
@@ -40,27 +36,19 @@ while game_should_continue:
     print(vs)
     print(f"Against B: {format_data(account_b)}.")
 
-    # Ask user for a guess.
     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-    # Clear the screen
     print("\n" * 20)
     print(logo)
 
-    # - Get follower count of each account
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
 
-    # Check if user is correct.
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-    # Give user feedback on their guess.
-    # score keeping.
     if is_correct:
         score += 1
         print(f"You're right! Current score {score}")
     else:
         print(f"Sorry, that's wrong. Final score: {score}.")
         game_should_continue = False
-
-
