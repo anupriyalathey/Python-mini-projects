eb45b023806af5542146cea50773e35cd62ae665
anupriyalathey <anupriyalathey@gmail.com>
2025-03-06 16:37:53 +0530

    added project 3 ui

--- 3. Rock_Paper_Scissors.py
+++ 3. Rock_Paper_Scissors.py
@@ -1,5 +1,62 @@
+# import random
+
+# rock = '''
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# '''
+
+# paper = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# '''
+
+# scissors = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
+
+# game_images = [rock, paper, scissors]
+
+# user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+# # Note: it's worth checking if the user has made a valid choice before the next line of code.
+# # If the user typed somthing other than 0, 1 or 2 the next line will give you an error.
+# # You could for example write:
+# if user_choice >= 0 and user_choice <= 2:
+#     print(game_images[user_choice])
+
+# computer_choice = random.randint(0, 2)
+# print("Computer chose:")
+# print(game_images[computer_choice])
+
+# if user_choice >= 3 or user_choice < 0:
+#     print("You typed an invalid number. You lose!")
+# elif user_choice == 0 and computer_choice == 2:
+#     print("You win!")
+# elif computer_choice == 0 and user_choice == 2:
+#     print("You lose!")
+# elif computer_choice > user_choice:
+#     print("You lose!")
+# elif user_choice > computer_choice:
+#     print("You win!")
+# elif computer_choice == user_choice:
+#     print("It's a draw!")
+
+import streamlit as st
 import random
 
+# ASCII art for the game
 rock = '''
     _______
 ---'   ____)
@@ -29,26 +86,32 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-# Note: it's worth checking if the user has made a valid choice before the next line of code.
-# If the user typed somthing other than 0, 1 or 2 the next line will give you an error.
-# You could for example write:
-if user_choice >= 0 and user_choice <= 2:
-    print(game_images[user_choice])
+def main():
+    st.title("🪨📄✂️ Rock, Paper, Scissors Game 🪨📄✂️")
+    st.write("Welcome to the classic game of Rock, Paper, Scissors!")
 
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
+    # User choice
+    user_choice = st.selectbox("What do you choose?", ["Rock", "Paper", "Scissors"])
+    user_choice_index = ["Rock", "Paper", "Scissors"].index(user_choice)
 
-if user_choice >= 3 or user_choice < 0:
-    print("You typed an invalid number. You lose!")
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-    print("You lose!")
-elif computer_choice > user_choice:
-    print("You lose!")
-elif user_choice > computer_choice:
-    print("You win!")
-elif computer_choice == user_choice:
-    print("It's a draw!")
+    # Display user's choice
+    st.write("You chose:")
+    st.code(game_images[user_choice_index])
+
+    # Computer choice
+    computer_choice = random.randint(0, 2)
+    st.write("Computer chose:")
+    st.code(game_images[computer_choice])
+
+    # Determine the result
+    if user_choice_index == computer_choice:
+        st.info("It's a draw!")
+    elif (user_choice_index == 0 and computer_choice == 2) or \
+         (user_choice_index == 1 and computer_choice == 0) or \
+         (user_choice_index == 2 and computer_choice == 1):
+        st.success("You win!")
+    else:
+        st.error("You lose!")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Below are the deployed links for the projects:
 
 1. [Tip Calculator](https://tip-splitter.streamlit.app/)
+2. [Treasure Island Game](https://treasure-island-game.streamlit.app/)
